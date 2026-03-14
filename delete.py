f4eabab314e5b6f7c5c0ea33defe998bb2317d91
Konstantin-sup <konstantin.artemovich.yudin18.ua@gmail.com>
2026-03-14 15:16:25 +0100

    added comments to file

--- delete.py
+++ delete.py
@@ -1,7 +1,7 @@
 def del_from_db(session, id: int):
-    """No commit, so i don't accidentally delete sme data(even from test base)"""
-    user = session.get(User, id)
-    user_protected = session.get(Protected, id)
+    """No commit, so i don't accidentally delete some data(even from test base)"""
+    user = session.get(User, id)  #getting user with target id from 'User' base(Model)
+    user_protected = session.get(Protected, id)  #same operation but from 'Protected' base(Model)
 
     if not user:
         raise HTTPException(
